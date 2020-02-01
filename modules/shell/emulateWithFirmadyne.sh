c4f1a52ba3674610cfbc09d30ecc49adee4d0cfd
Michael Kraus <mike.kraus@onlinehome.de>
2020-02-01 19:20:05 +0100

    some advantages

--- modules/shell/emulateWithFirmadyne.sh
+++ modules/shell/emulateWithFirmadyne.sh
@@ -2,6 +2,5 @@
 echo "# [start]: firmadyne emulation"
 cd /home/admiral-helmut/MA/tools/firmware-analysis-toolkit/firmadyne
 echo $2
-echo xapioMK616 | sudo -S
 go run main.go $2
 echo "# [finisched]: firmadyne emulation"
--- modules/shell/startNmapScan.sh
+++ modules/shell/startNmapScan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "# [start]: Network interface scan (nmap)"
 cd /home/admiral-helmut/MA/efi4st/modules/python
-sudo nmap -p 1-1000 -sV -sS 192.168.0.100 | python ../../modules/python/pipeResultsSendToServer.py $1 "NMAPNetworkInterfaces" ""
+sudo nmap -p 1-1000 -sV -sS XXX.XXX.XXX.XXX | python ../../modules/python/pipeResultsSendToServer.py $1 "NMAPNetworkInterfaces" ""
 echo "# [finisched]:  Network interface scan (nmap)"
 
